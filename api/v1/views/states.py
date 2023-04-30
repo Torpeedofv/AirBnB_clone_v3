5ad2070aeb46303912c92a7d72091225dc07e127
Torpeedofv <torpeedo.fv@gmail.com>
2023-04-30 12:44:30 -0700

    commit for task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 """a blueprint module that handles all default RESTFul API actions"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response
 from models import storage
 from models.state import State
 
 
-@app_views.route('/states')
+@app_views.route('/states', strict_slashes=False)
 def all_states():
     return jsonify([state.to_dict()
                    for state in storage.all(State).values()])
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
+def state_object(state_id):
+    if storage.get(State, state_id) is None:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify(storage.get(State, state_id).to_dict())
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['DELETE'])
+def delete_state(state_id):
+    
