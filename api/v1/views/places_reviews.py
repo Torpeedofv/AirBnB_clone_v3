0b6c55d020ebd0d58ec0a92203bf921c88bdd793
Torpeedofv <torpeedo.fv@gmail.com>
2023-05-01 10:35:02 -0700

    commit

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -5,6 +5,7 @@ from flask import abort, request, jsonify, make_response
 from models import storage
 from models.place import Place
 from models.review import Review
+from models.user import User
 
 
 @app_views.route('/places/<place_id>/reviews', strict_slashes=False,
