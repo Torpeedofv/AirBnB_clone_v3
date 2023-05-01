5d2bf458309d54ec662464529f9c907f2b672661
Torpeedofv <torpeedo.fv@gmail.com>
2023-05-01 09:03:50 -0700

    task 9 correction

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import abort, request, jsonify, make_response
 from models import storage
 from models.amenity import Amenity
-
+from models.user import User
 
 @app_views.route('/users', strict_slashes=False, methods=['GET', 'POST'])
 def post_get():
