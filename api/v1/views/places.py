19f7ce184e3123b6db06f6e109089e9e44ac173e
Torpeedofv <torpeedo.fv@gmail.com>
2023-05-01 10:19:41 -0700

    commit

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,15 +7,16 @@ from models.place import Place
 from models.city import City
 
 
-@app_views.route('/cities/<city_id>/places', strict_slashes=False, methods=['GET', 'POST'])
+@app_views.route('/cities/<city_id>/places', strict_slashes=False,
+                 methods=['GET', 'POST'])
 def get_post(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    
+
     if request.method == 'GET':
         return jsonify([place.to_dict() for place in city.places])
-    
+
     if request.method == 'POST':
         if not request.get_json():
             abort(400, 'Not a JSON')
@@ -27,12 +28,13 @@ def get_post(city_id):
             abort(404)
         json_data = request.get_json()
         place = Place(user_id=json_data['user_id'], name=json_data['name'])
-        storage.new(place)
+        place.city_id = city_id
         place.save()
         return make_response(jsonify(place.to_dict()), 201)
 
 
-@app_views.route('/places/<place_id>', strict_slashes=False, methods=['GET', 'DELETE', 'PUT'])
+@app_views.route('/places/<place_id>', strict_slashes=False,
+                 methods=['GET', 'DELETE', 'PUT'])
 def all_methods(place_id):
     place = storage.get(Place, place_id)
     if not place:
@@ -49,7 +51,8 @@ def all_methods(place_id):
         if not request.get_json():
             abort(400, 'Not a JSON')
         for key, value in request.get_json().items():
-            if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
+            if key not in ['id', 'user_id', 'city_id',
+                           'created_at', 'updated_at']:
                 setattr(place, key, value)
         place.save()
         return make_response(jsonify(place.to_dict()), 200)
